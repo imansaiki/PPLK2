f37803e23896b3290bfc600ec1f4c25692e9b3f4
imansaiki <imansaiki@users.noreply.github.com>
2018-12-09 16:33:41 +0700

    add data lowongan model

--- application/models/lowonganM.php
+++ application/models/lowonganM.php
@@ -24,8 +24,13 @@ class LowonganM extends CI_Model
   function getLowongan($id)
   {
     $this->db->flush_cache();
+    $this->db->select('loker.idloker,loker.nama,loker.idperusahaan,perusahaan.nama as namaperusahaan,loker.tipe,loker.gaji_min,loker.gaji_max,loker.deskripsi_loker,loker.nama_cp,loker.email_cp,loker.no_telp_cp,loker.tgl_expired,tingkat_pendidikan.keterangan as pendidikan,bidang_pekerjaan.nama as bidang,perusahaan.kota,perusahaan.alamat');
     $this->db->where('idloker',$id);
-    $query=$this->db->get('loker');
+    $this->db->from('loker');
+    $this->db->join('perusahaan', 'perusahaan.idperusahaan = loker.idperusahaan');
+    $this->db->join('tingkat_pendidikan', 'tingkat_pendidikan.idtingkat_pendidikan = loker.idtingkat_pendidikan');
+    $this->db->join('bidang_pekerjaan', 'bidang_pekerjaan.idbidang = loker.idbidang');
+    $query=$this->db->get();
     return $query->result_array();
   }
   function getListLowongan(){
--- application/views/detailLowongan.php
+++ application/views/detailLowongan.php
@@ -8,15 +8,35 @@
       <div class="row">
         <div class="col l8">
           <div class="card-panel">
+            <h5>Deskripsi Pekerjaan</h5>
+            <div class="divider"></div>
             <?php echo $list[0]['deskripsi_loker'] ?>
           </div>
         </div>
         <div class="col l4">
           <div class="card-panel">
-            Gaji : <?php echo $list[0]['gaji_min'].' - '.$list[0]['gaji_max'] ?>
+            <h6>Syarat Umum</h6>
+            <div class="divider"></div>
+            Pendidikan : <?php echo $list[0]['pendidikan']?><br>
+            Bidang : <?php echo $list[0]['bidang'] ?><br>
+            Gaji : <?php echo $list[0]['gaji_min'].' - '.$list[0]['gaji_max'] ?><br>
+            Tanggal Tutup : <?php echo $list[0]['tgl_expired'] ?>
           </div>
           <div class="card-panel">
-            Gaji : <?php echo $list[0]['gaji_min'].' - '.$list[0]['gaji_max'] ?>
+            <div class="section">
+              <h6>Data Perusahaan</h6>
+              <div class="divider"></div>
+              <?php echo $list[0]['namaperusahaan'] ?><br>
+              Kota : <?php echo $list[0]['namaperusahaan'] ?><br>
+              Alamat : <?php echo $list[0]['alamat'] ?><br>
+            </div>
+            <div class="section">
+              <h6>Kontak</h6>
+              <div class="divider"></div>
+              Nama : <?php echo $list[0]['nama_cp'] ?><br>
+              Email : <?php echo $list[0]['email_cp'] ?><br>
+              Telepon : <?php echo $list[0]['no_telp_cp'] ?><br>
+            </div>
           </div>
         </div>
       </div>
